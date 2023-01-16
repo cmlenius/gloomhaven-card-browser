411a111043f3a17586dc671763bdfc22bd0e7015
cmlenius <cameron.lenius@gmail.com>
2023-01-16 22:04:36 +0100

    Add vercel analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,12 @@
 import "../public/global.css";
+import { Analytics } from "@vercel/analytics/react";
 import { SpoilersProvider } from "../hooks/useSpoilers";
 
 function MyApp({ Component, pageProps }) {
   return (
     <SpoilersProvider>
       <Component {...pageProps} />
+      <Analytics />
     </SpoilersProvider>
   );
 }
