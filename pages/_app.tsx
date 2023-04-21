f8515ff30ae8022ee4200164d2b3700e4dc4af3a
cmlenius <cameron.lenius@gmail.com>
2023-04-21 22:49:54 +0200

    Remove analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,10 @@
 import "../public/global.css";
-import { Analytics } from "@vercel/analytics/react";
 import { SpoilersProvider } from "../hooks/useSpoilers";
 
 function MyApp({ Component, pageProps }) {
   return (
     <SpoilersProvider>
       <Component {...pageProps} />
-      <Analytics />
     </SpoilersProvider>
   );
 }
